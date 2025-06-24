e46423319675a7c8a4f170b95458e9b5b49d612e
Florian BILY <fbily@f2r11s9.clusters.42paris.fr>
2025-06-24 18:11:14 +0200

    Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "cloud1_sg" {
+  name        = "cloud1-security-group"
+  description = "Allow inbound SSH/HTTP/HTTPS and all outbound"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.cloud1_sg.id
+  description       = "SSH from anywhere"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.cloud1_sg.id
+  description       = "HTTP from anywhere"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.cloud1_sg.id
+  description       = "HTTPS from anywhere"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "all_outbound" {
+  security_group_id = aws_security_group.cloud1_sg.id
+  description       = "Allow all outbound traffic"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_instance" "cloud1" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.cloud1_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Cloud1-WordPress-Server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  default = "eu-west-3" # Paris
+}
+
+variable "key_name" {
+  description = "Nom de la clé SSH dans AWS"
+  default = "Cloud1-SSH"
+}
+
+variable "public_key_path" {
+  description = "Chemin absolu vers la clé publique locale"
+  default     = "/home/fbily/.ssh/id_rsa.pub"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI Ubuntu 20.04 LTS pour Paris (eu-west-3)"
+  default = "ami-0faa5b34db2bd357f"
+}
